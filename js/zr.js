4dc6b41fa04aa63f5a4a91d03a4c2c29991bafe5
mmbbhh <mmbbbhhh@126.com>
2016-12-09 21:16:35 +0800

    Add files via upload

--- js/zr.js
+++ js/zr.js
@@ -1,9 +1,6 @@
 var welcome = prompt("欢迎来访，告诉我你的名字");
-if(welcome !=null && welcome !="")
 {alert("欢迎你,"+welcome);
-alert("版权所有（大概也许）")
+alert("版权所有（大概也许）1615班马斌恒")
 alert("主要工具：sublime hbulider")
 alert("可用支链，学校新闻板块中六个新闻")
 alert("github分支：https://mmbbhh.github.io/Eve/")}
-else{alert("不说不让进！");
-window.close();}
